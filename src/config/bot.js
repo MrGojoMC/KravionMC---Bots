779c48a0200bfa38cf30cd96787bfabfb8a146f6
ZyphryxMC <nishmc.ontop@gmail.com>
2026-08-15 02:21:40 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,8 +23,8 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
-        state: "stalking",     // this is what people actually see
+        name: "KravionMC Network", // required by Discord API, not shown in the client
+        state: "kravionmc.net",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
